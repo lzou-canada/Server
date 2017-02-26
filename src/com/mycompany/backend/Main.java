d68b4139c2eceed9abb3d0a8c4b0a22270fe51ce
lzou-canada <lizou.canada@gmail.com>
2017-02-26 13:06:41 -0500

    Initial checkin

---
+++ src/com/mycompany/backend/Main.java
@@ -0,0 +1,12 @@
+package com.mycompany.backend;
+
+/**
+ * Created by Fei Chen on 2/26/2017.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
