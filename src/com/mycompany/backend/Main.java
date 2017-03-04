31fed161aa1116d65d13676f9b1510b942a25449
lzou-canada <lizou.canada@gmail.com>
2017-03-04 12:48:11 -0500

    Add line B2 to move the master

--- src/com/mycompany/backend/Main.java
+++ src/com/mycompany/backend/Main.java
@@ -7,6 +7,7 @@ public class Main
 {
     public static void main(String[] args)
     {
+        // add line B2 to move the master
         // add line B1 to move the master
         // add one line to the Server module
         // add one line here
