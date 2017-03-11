2bd5ca4c3a991f47a26fa92b35ac679544792e2d
lzou-canada <lizou.canada@gmail.com>
2017-03-10 22:14:32 -0500

    Do something else

--- src/com/mycompany/backend/Main.java
+++ src/com/mycompany/backend/Main.java
@@ -11,25 +11,9 @@ public class Main
     // add line B5 to move the master
     public static void main(String[] args)
     {
-        // add line A5 to move the PTS-200
-        // add line A4 to move the PTS-200
-        // add line B3 to move the master
-        // add line A2 to move the PTS-200
-        // add line A1 to move the master
-        // add line B2 to move the master
-        // add line B1 to move the master
-        // add one line to the Server module
-        // add one line here to test the file status and color indicator
-        // add one line to modify the file and test its color changes
         int a = 15;
         int b = 20;
 
-        // add line E2 to further move the master
-        // add line E1 to move the master
-        // add line B5 to further move the master
-        // add line B4 to move the master
-        // add A2 to move the PTS-200
-        // add A1 to move the PTS-200
         for ( int i = 0; i < a; i++ )
         {
             for ( int j = 0; j < b; j++ )
