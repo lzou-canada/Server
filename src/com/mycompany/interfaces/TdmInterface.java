e9012006ff5d321d6b4ab50589c56bc32d6fb2a7
lzou-canada <lizou.canada@gmail.com>
2017-05-02 19:09:25 -0400

    Test conflict issue

--- src/com/mycompany/interfaces/TdmInterface.java
+++ src/com/mycompany/interfaces/TdmInterface.java
@@ -7,6 +7,7 @@ public class TdmInterface extends BaseInterface
 {
     public TdmInterface(String name)
     {
+        // add one line of comment her to test conflict
         super(name, InterfaceType.INTERFACE_TYPE_TDM);
     }
 }
